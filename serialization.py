5090143d35efdead27f404e501d322e4531fe5e1
Alpaca <70183390+DatDarkAlpaca@users.noreply.github.com>
2021-12-11 03:49:51 -0400

    Got rid of redundant code.

--- serialization.py
+++ serialization.py
@@ -36,21 +36,32 @@ def check_for_saves():
 # Buddy file:
 class BuddyFile:
     def __init__(self, name='', profile_path='', buddy_path=''):
-        # Profile Picture:
-        image_reader = QImageReader(profile_path)
-        if image_reader.supportsAnimation() and image_reader.imageCount() > 1:
-            self.profile_picture = convert_gif_into_frames(profile_path)
-        else:
-            self.profile_picture = [QImage(profile_path)]
-
-        # Mini Buddy Picture:
-        image_reader = QImageReader(buddy_path)
-        if image_reader.supportsAnimation() and image_reader.imageCount() > 1:
-            self.buddy_picture = convert_gif_into_frames(buddy_path)
-        else:
-            self.buddy_picture = [QImage(buddy_path)]
-
+        self.profile_picture = BuddyFile.determine_file_type(profile_path)
+        self.buddy_picture = BuddyFile.determine_file_type(buddy_path)
         self.name = name
+
+    @staticmethod
+    def determine_file_type(filepath):
+        image_reader = QImageReader(filepath)
+        if image_reader.supportsAnimation() and image_reader.imageCount() > 1:
+            return convert_gif_into_frames(filepath)
+        else:
+            return QImage(filepath)
+
+
+# Serialization:
+def write_images(out, picture):
+    if isinstance(picture, QImage):
+        out.writeInt8(0x0A)
+        _ = out << picture
+
+    elif isinstance(picture, list):
+        mini_buddy_frame_size = len(picture)
+
+        out.writeInt8(0x0B)
+        out.writeInt16(mini_buddy_frame_size)
+        for frame in picture:
+            _ = out << frame
 
 
 def save_buddy(buddy_file: BuddyFile, filename):
@@ -68,43 +79,36 @@ def save_buddy(buddy_file: BuddyFile, filename):
     out.writeString(buddy_file.name)
 
     # Profile Picture(s):
-    profile_frame_size = len(buddy_file.profile_picture)
-    if profile_frame_size == 1:
-        out.writeInt8(0x0A)
-        if isinstance(buddy_file.profile_picture[0], QImage):
-            out << buddy_file.profile_picture[0]
-        else:
-            raise Exception('When saving single image, received an invalid profile picture type.')
-    else:
-        out.writeInt8(0x0B)
-        if isinstance(buddy_file.profile_picture, list):
-            out.writeInt16(profile_frame_size)
-            for frame in buddy_file.profile_picture:
-                out << frame
-        else:
-            raise Exception('When saving multiple images, received an invalid profile picture type.')
+    write_images(out, buddy_file.profile_picture)
 
     # Buddy Picture(s):
-    mini_buddy_frame_size = len(buddy_file.buddy_picture)
-    if mini_buddy_frame_size == 1:
-        out.writeInt8(0x0A)
-        if isinstance(buddy_file.buddy_picture[0], QImage):
-            out << buddy_file.buddy_picture[0]
-        else:
-            raise Exception('When saving single image, received an invalid profile picture type.')
-    else:
-        out.writeInt8(0x0B)
-        if isinstance(buddy_file.buddy_picture, list):
-            out.writeInt16(mini_buddy_frame_size)
-            for frame in buddy_file.buddy_picture:
-                out << frame
-        else:
-            raise Exception('When saving multiple images, received an invalid profile picture type.')
+    write_images(out, buddy_file.buddy_picture)
 
     file.close()
 
 
 # Load:
+def load_images(in_file):
+    image_type = in_file.readInt8()
+    print(hex(image_type))
+    if image_type == 0x0A:
+        image = QImage()
+        in_file >> image
+
+    elif image_type == 0x0B:
+        frame_count = in_file.readInt16()
+        image = []
+        for _ in range(frame_count):
+            temp_image = QImage()
+            in_file >> temp_image
+
+            image.append(temp_image)
+    else:
+        raise Exception('Buddy type not found.')
+
+    return image
+
+
 def load_buddy(filename=None):
     if not filename:
         filename = check_for_saves()
@@ -118,45 +122,19 @@ def load_buddy(filename=None):
     file.open(QIODevice.ReadOnly)
 
     # Stream & Magic Number & Version:
-    out = QDataStream(file)
-    magic = out.readInt32()
+    in_file = QDataStream(file)
+    magic = in_file.readInt32()
 
     if not magic == 0x0000C0D1:
         raise Exception('Version or validation number mismatch.')
 
     # Name:
-    name = out.readString()
+    name = in_file.readString()
 
     # Profile Picture:
-    profile_type = out.readInt8()
-    if profile_type == 0x0A:
-        profile_image = QImage()
-        out >> profile_image
-    elif profile_type == 0x0B:
-        frame_count = out.readInt16()
-        profile_image = []
-        for _ in range(frame_count):
-            image = QImage()
-            out >> image
+    profile_image = load_images(in_file)
 
-            profile_image.append(image)
-    else:
-        raise Exception('Profile type not found.')
-
-    # Profile Picture:
-    buddy_type = out.readInt8()
-    if buddy_type == 0x0A:
-        buddy_image = QImage()
-        out >> buddy_image
-    elif buddy_type == 0x0B:
-        frame_count = out.readInt16()
-        buddy_image = []
-        for _ in range(frame_count):
-            image = QImage()
-            out >> image
-
-            buddy_image.append(image)
-    else:
-        raise Exception('Buddy type not found.')
+    # Buddy Picture:
+    buddy_image = load_images(in_file)
 
     return {'name': name, 'profile_picture': profile_image, 'mini_buddy_picture': buddy_image}
